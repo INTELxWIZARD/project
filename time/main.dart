61c43b54e4114533e185927f3ea6ca6f11c174a2
HILALKALAM A <167734341+INTELxWIZARD@users.noreply.github.com>
2025-01-17 19:39:51 +0530

    Add files via upload

--- time/main.dart
+++ time/main.dart
@@ -1,1 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:omni_datetime_picker/omni_datetime_picker.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Omni DateTime Picker',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.cyan,
+          brightness: Brightness.light,
+        ),
+      ),
+      home: const OmniExample(),
+    );
+  }
+}
+
+class OmniExample extends StatelessWidget {
+  const OmniExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Omni DateTime Picker Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                final DateTime? dateTime =
+                    await showOmniDateTimePicker(context: context);
+
+                // Use dateTime here
+                debugPrint('Selected DateTime: $dateTime');
+              },
+              child: const Text('Show DateTime Picker'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                final List<DateTime>? dateTime =
+                    await showOmniDateTimeRangePicker(context: context);
+
+                // Use dateTime here
+                debugPrint('Selected DateTime Range: $dateTime');
+              },
+              child: const Text('Show DateTime Range Picker'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
